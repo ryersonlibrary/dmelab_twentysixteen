a001d7787869f0f21e0b8738ed39ee3f586aa27f
Patrick Fung <patrick@makestuffdostuff.com>
2016-10-03 11:33:36 -0400

    remove uncompatible lines from 2.9.x

--- inc/ninja_forms_customization.php
+++ inc/ninja_forms_customization.php
@@ -4,15 +4,3 @@
  *
  * Custom stuff for RULA :D
  */
-
-// Register custom sidebar for ninja forms :D
-function ninja_forms_register_sidebar_rula_google_sheets(){
-  $args = array(
-    'name' => __( 'RULA Google Sheets', 'ninja-forms' ),
-    'page' => 'ninja-forms', 
-    'tab' => 'builder',
-    'display_function' => 'ninja_forms_sidebar_display_fields'
-  );
-  ninja_forms_register_sidebar('rula_google_sheets', $args);
-}
-add_action('admin_init', 'ninja_forms_register_sidebar_rula_google_sheets');
