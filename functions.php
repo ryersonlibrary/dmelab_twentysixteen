6a6b11b38529ebdb14c55763ac7643002da40451
Patrick Fung <patrick@makestuffdostuff.com>
2016-10-18 14:52:05 -0400

    Add custom options page to theme

--- functions.php
+++ functions.php
@@ -166,3 +166,10 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Add Advanced Custom Fields custom options page.
+ */
+if ( function_exists('acf_add_options_page') ) {
+	acf_add_options_page();	
+}
